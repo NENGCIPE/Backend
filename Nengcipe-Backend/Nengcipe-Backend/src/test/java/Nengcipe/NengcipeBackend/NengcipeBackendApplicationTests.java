941d7a7bfecb66e162a51a88eaaef2956bafecbe
seongmin <growth_s@naver.com>
2023-05-03 12:56:56 +0900

    validation 적용

--- Nengcipe-Backend/Nengcipe-Backend/src/test/java/Nengcipe/NengcipeBackend/NengcipeBackendApplicationTests.java
+++ Nengcipe-Backend/Nengcipe-Backend/src/test/java/Nengcipe/NengcipeBackend/NengcipeBackendApplicationTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(locations = "classpath:application-test.properties")
 class NengcipeBackendApplicationTests {
 
 	@Test
